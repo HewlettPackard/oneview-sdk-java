e90960912e31d9f5979ea30df247688255b91aa8
Sood <priyanka.sood@hpe.com>
2017-11-10 15:19:05 +0530

    Removing BaseModelResource inheritance and adding only those attributes in dto which are required

--- oneview-sdk-java-lib/src/main/java/com/hp/ov/sdk/dto/security/login/LoginDetail.java
+++ oneview-sdk-java-lib/src/main/java/com/hp/ov/sdk/dto/security/login/LoginDetail.java
@@ -1,54 +1,201 @@
+/*
+ * (C) Copyright 2017 Hewlett Packard Enterprise Development LP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hp.ov.sdk.dto.security.login;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
-import com.hp.ov.sdk.dto.BaseModelResource;
 
-public class LoginDetail extends BaseModelResource{
-	
-	private static final long serialVersionUID = 1L;
+import com.google.gson.GsonBuilder;
 
-	private List<LoginDomain> configuredLoginDomains = new ArrayList<LoginDomain>();
-	private String defaultLoginDomain;
-	private LoginMessage loginMessage;
-	private Boolean allowLocalLogin;
-	private Boolean technicianEnabled;	
-	
-	public List<LoginDomain> getConfiguredLoginDomains() {
-		return configuredLoginDomains;
-	}
-	public void setConfiguredLoginDomains(List<LoginDomain> configuredLoginDomains) {
-		this.configuredLoginDomains = configuredLoginDomains;
-	}
-	public String getDefaultLoginDomain() {
-		return defaultLoginDomain;
-	}
-	public void setDefaultLoginDomain(String defaultLoginDomain) {
-		this.defaultLoginDomain = defaultLoginDomain;
-	}
-	public LoginMessage getLoginMessage() {
-		return loginMessage;
-	}
-	public void setLoginMessage(LoginMessage loginMessage) {
-		this.loginMessage = loginMessage;
-	}
-	public Boolean getAllowLocalLogin() {
-		return allowLocalLogin;
-	}
-	public void setAllowLocalLogin(Boolean allowLocalLogin) {
-		this.allowLocalLogin = allowLocalLogin;
-	}
-	public Boolean getTechnicianEnabled() {
-		return technicianEnabled;
-	}
-	public void setTechnicianEnabled(Boolean technicianEnabled) {
-		this.technicianEnabled = technicianEnabled;
-	}
-	
-	@Override
+public class LoginDetail implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private String type;
+    private String defaultLoginDomain;
+    private List<LoginDomain> configuredLoginDomains = new ArrayList<LoginDomain>();
+    private Boolean allowLocalLogin;
+    private Boolean technicianEnabled;
+    private LoginMessage loginMessage;
+    private String eTag;
+    private Date created;
+    private Date modified;
+    private String category;   
+    private String uri;
+    
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+    
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    /**
+     * @return the defaultLoginDomain
+     */
+    public String getDefaultLoginDomain() {
+        return defaultLoginDomain;
+    }
+    
+    /**
+     * @param defaultLoginDomain the defaultLoginDomain to set
+     */
+    public void setDefaultLoginDomain(String defaultLoginDomain) {
+        this.defaultLoginDomain = defaultLoginDomain;
+    }
+    
+    /**
+     * @return the configuredLoginDomains
+     */
+    public List<LoginDomain> getConfiguredLoginDomains() {
+        return configuredLoginDomains;
+    }
+
+    /**
+     * @param configuredLoginDomains the configuredLoginDomains to set
+     */
+    public void setConfiguredLoginDomains(List<LoginDomain> configuredLoginDomains) {
+        this.configuredLoginDomains = configuredLoginDomains;
+    }
+    
+    /**
+     * @return the allowLocalLogin
+     */
+    public Boolean getAllowLocalLogin() {
+        return allowLocalLogin;
+    }
+    
+    /**
+     * @param allowLocalLogin the allowLocalLogin to set
+     */
+    public void setAllowLocalLogin(Boolean allowLocalLogin) {
+        this.allowLocalLogin = allowLocalLogin;
+    }
+    
+    /**
+     * @return the technicianEnabled
+     */
+    public Boolean getTechnicianEnabled() {
+        return technicianEnabled;
+    }
+    
+    /**
+     * @param technicianEnabled the technicianEnabled to set
+     */
+    public void setTechnicianEnabled(Boolean technicianEnabled) {
+        this.technicianEnabled = technicianEnabled;
+    }
+    
+    /**
+     * @return the loginMessage
+     */
+    public LoginMessage getLoginMessage() {
+        return loginMessage;
+    }
+    
+    /**
+     * @param loginMessage the loginMessage to set
+     */
+    public void setLoginMessage(LoginMessage loginMessage) {
+        this.loginMessage = loginMessage;
+    }
+    
+    /**
+     * @return the eTag
+     */
+    public String geteTag() {
+        return eTag;
+    }
+
+    /**
+     * @param eTag the eTag to set
+     */
+    public void seteTag(String eTag) {
+        this.eTag = eTag;
+    }
+    
+    /**
+     * @return the created
+     */
+    public Date getCreated() {
+        return created;
+    }
+
+    /**
+     * @param created the created to set
+     */
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+    
+    /**
+     * @return the modified
+     */
+    public Date getModified() {
+        return modified;
+    }
+
+    /**
+     * @param modified the modified to set
+     */
+    public void setModified(Date modified) {
+        this.modified = modified;
+    }
+    
+    /**
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+    
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+    /**
+     * @return the uri
+     */
+    public String getUri() {
+        return uri;
+    }
+
+    /**
+     * @param uri the uri to set
+     */
+    public void setUri(String uri) {
+        this.uri = uri;
+    }    
+
+    @Override
     public String toString() {
         return ToStringBuilder.reflectionToString(this);
     }
@@ -62,6 +209,12 @@ public class LoginDetail extends BaseModelResource{
     public int hashCode() {
         return HashCodeBuilder.reflectionHashCode(this);
     }
-	
-	
+    
+    public String toJsonString() {
+        return System.getProperty("line.separator")
+            + new GsonBuilder()
+                .setPrettyPrinting()
+                .disableHtmlEscaping()
+                .create().toJson(this);
+    }
 }
--- oneview-sdk-java-lib/src/main/java/com/hp/ov/sdk/dto/security/login/LoginMessage.java
+++ oneview-sdk-java-lib/src/main/java/com/hp/ov/sdk/dto/security/login/LoginMessage.java
@@ -1,35 +1,153 @@
+/*
+ * (C) Copyright 2017 Hewlett Packard Enterprise Development LP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hp.ov.sdk.dto.security.login;
+
+import java.io.Serializable;
+import java.util.Date;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
-import com.hp.ov.sdk.dto.BaseModelResource;
 
-public class LoginMessage extends BaseModelResource{	
-	
-	
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -70715866435733249L;
-	private Boolean acknowledgment;
-	private String message;
-	
-	public Boolean getAcknowledgment() {
-		return acknowledgment;
-	}
-	public void setAcknowledgment(Boolean acknowledgment) {
-		this.acknowledgment = acknowledgment;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	
-	@Override
+public class LoginMessage implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private String type;
+    private Boolean acknowledgment;
+    private String message;
+    private String eTag;
+    private Date created;
+    private Date modified;
+    private String category;   
+    private String uri;
+    
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+    
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    /**
+     * @return the acknowledgment
+     */
+    public Boolean getAcknowledgment() {
+        return acknowledgment;
+    }
+    
+    /**
+     * @param acknowledgment the acknowledgment to set
+     */
+    public void setAcknowledgment(Boolean acknowledgment) {
+        this.acknowledgment = acknowledgment;
+    }
+    
+    /**
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+    
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * @return the eTag
+     */
+    public String geteTag() {
+        return eTag;
+    }
+
+    /**
+     * @param eTag the eTag to set
+     */
+    public void seteTag(String eTag) {
+        this.eTag = eTag;
+    }
+    
+    /**
+     * @return the created
+     */
+    public Date getCreated() {
+        return created;
+    }
+
+    /**
+     * @param created the created to set
+     */
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+    
+    /**
+     * @return the modified
+     */
+    public Date getModified() {
+        return modified;
+    }
+
+    /**
+     * @param modified the modified to set
+     */
+    public void setModified(Date modified) {
+        this.modified = modified;
+    }
+    
+    /**
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+    
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+    /**
+     * @return the uri
+     */
+    public String getUri() {
+        return uri;
+    }
+
+    /**
+     * @param uri the uri to set
+     */
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    @Override
     public String toString() {
         return ToStringBuilder.reflectionToString(this);
     }
@@ -43,4 +161,5 @@ public class LoginMessage extends BaseModelResource{
     public int hashCode() {
         return HashCodeBuilder.reflectionHashCode(this);
     }
+    
 }
