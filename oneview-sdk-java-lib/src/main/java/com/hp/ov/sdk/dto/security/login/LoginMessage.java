026c15e2fc87ab3a0ebea1cd716fa79daff194ae
Sood <priyanka.sood@hpe.com>
2017-11-23 19:57:30 +0530

    Modified LoginDTOs to implement BaseLoginResource

--- oneview-sdk-java-lib/src/main/java/com/hp/ov/sdk/dto/security/login/LoginMessage.java
+++ oneview-sdk-java-lib/src/main/java/com/hp/ov/sdk/dto/security/login/LoginMessage.java
@@ -15,136 +15,45 @@
  */
 package com.hp.ov.sdk.dto.security.login;
 
-import java.io.Serializable;
-import java.util.Date;
-
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-public class LoginMessage implements Serializable {
+public class LoginMessage extends BaseLoginResource {
 
     private static final long serialVersionUID = 1L;
-    
-    private String type;
+
     private Boolean acknowledgment;
     private String message;
-    private String eTag;
-    private Date created;
-    private Date modified;
-    private String category;   
-    private String uri;
-    
-    /**
-     * @return the type
-     */
-    public String getType() {
-        return type;
-    }
-    
-    /**
-     * @param type the type to set
-     */
-    public void setType(String type) {
-        this.type = type;
-    }
-    
+
     /**
      * @return the acknowledgment
      */
     public Boolean getAcknowledgment() {
         return acknowledgment;
     }
-    
+
     /**
-     * @param acknowledgment the acknowledgment to set
+     * @param acknowledgment
+     *            the acknowledgment to set
      */
     public void setAcknowledgment(Boolean acknowledgment) {
         this.acknowledgment = acknowledgment;
     }
-    
+
     /**
      * @return the message
      */
     public String getMessage() {
         return message;
     }
-    
+
     /**
-     * @param message the message to set
+     * @param message
+     *            the message to set
      */
     public void setMessage(String message) {
         this.message = message;
-    }
-
-    /**
-     * @return the eTag
-     */
-    public String geteTag() {
-        return eTag;
-    }
-
-    /**
-     * @param eTag the eTag to set
-     */
-    public void seteTag(String eTag) {
-        this.eTag = eTag;
-    }
-    
-    /**
-     * @return the created
-     */
-    public Date getCreated() {
-        return created;
-    }
-
-    /**
-     * @param created the created to set
-     */
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-    
-    /**
-     * @return the modified
-     */
-    public Date getModified() {
-        return modified;
-    }
-
-    /**
-     * @param modified the modified to set
-     */
-    public void setModified(Date modified) {
-        this.modified = modified;
-    }
-    
-    /**
-     * @return the category
-     */
-    public String getCategory() {
-        return category;
-    }
-    
-    /**
-     * @param category the category to set
-     */
-    public void setCategory(String category) {
-        this.category = category;
-    }
-    
-    /**
-     * @return the uri
-     */
-    public String getUri() {
-        return uri;
-    }
-
-    /**
-     * @param uri the uri to set
-     */
-    public void setUri(String uri) {
-        this.uri = uri;
     }
 
     @Override
@@ -161,5 +70,5 @@ public class LoginMessage implements Serializable {
     public int hashCode() {
         return HashCodeBuilder.reflectionHashCode(this);
     }
-    
+
 }
