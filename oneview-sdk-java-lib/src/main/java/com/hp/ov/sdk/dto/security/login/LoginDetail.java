3b1e4a9d9f506e959b997a18b490d562ba217e51
Sood <priyanka.sood@hpe.com>
2017-11-23 19:57:30 +0530

    Modified LoginDTOs to implement BaseLoginResource

--- oneview-sdk-java-lib/src/main/java/com/hp/ov/sdk/dto/security/login/LoginDetail.java
+++ oneview-sdk-java-lib/src/main/java/com/hp/ov/sdk/dto/security/login/LoginDetail.java
@@ -15,60 +15,40 @@
  */
 package com.hp.ov.sdk.dto.security.login;
 
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import com.google.gson.GsonBuilder;
 
-public class LoginDetail implements Serializable {
+public class LoginDetail extends BaseLoginResource {
 
     private static final long serialVersionUID = 1L;
-    
-    private String type;
+
     private String defaultLoginDomain;
     private List<LoginDomain> configuredLoginDomains = new ArrayList<LoginDomain>();
     private Boolean allowLocalLogin;
     private Boolean technicianEnabled;
     private LoginMessage loginMessage;
-    private String eTag;
-    private Date created;
-    private Date modified;
-    private String category;   
-    private String uri;
-    
-    /**
-     * @return the type
-     */
-    public String getType() {
-        return type;
-    }
-    
-    /**
-     * @param type the type to set
-     */
-    public void setType(String type) {
-        this.type = type;
-    }
-    
+
     /**
      * @return the defaultLoginDomain
      */
     public String getDefaultLoginDomain() {
         return defaultLoginDomain;
     }
-    
+
     /**
-     * @param defaultLoginDomain the defaultLoginDomain to set
+     * @param defaultLoginDomain
+     *            the defaultLoginDomain to set
      */
     public void setDefaultLoginDomain(String defaultLoginDomain) {
         this.defaultLoginDomain = defaultLoginDomain;
     }
-    
+
     /**
      * @return the configuredLoginDomains
      */
@@ -77,123 +57,57 @@ public class LoginDetail implements Serializable {
     }
 
     /**
-     * @param configuredLoginDomains the configuredLoginDomains to set
+     * @param configuredLoginDomains
+     *            the configuredLoginDomains to set
      */
     public void setConfiguredLoginDomains(List<LoginDomain> configuredLoginDomains) {
         this.configuredLoginDomains = configuredLoginDomains;
     }
-    
+
     /**
      * @return the allowLocalLogin
      */
     public Boolean getAllowLocalLogin() {
         return allowLocalLogin;
     }
-    
+
     /**
-     * @param allowLocalLogin the allowLocalLogin to set
+     * @param allowLocalLogin
+     *            the allowLocalLogin to set
      */
     public void setAllowLocalLogin(Boolean allowLocalLogin) {
         this.allowLocalLogin = allowLocalLogin;
     }
-    
+
     /**
      * @return the technicianEnabled
      */
     public Boolean getTechnicianEnabled() {
         return technicianEnabled;
     }
-    
+
     /**
-     * @param technicianEnabled the technicianEnabled to set
+     * @param technicianEnabled
+     *            the technicianEnabled to set
      */
     public void setTechnicianEnabled(Boolean technicianEnabled) {
         this.technicianEnabled = technicianEnabled;
     }
-    
+
     /**
      * @return the loginMessage
      */
     public LoginMessage getLoginMessage() {
         return loginMessage;
     }
-    
+
     /**
-     * @param loginMessage the loginMessage to set
+     * @param loginMessage
+     *            the loginMessage to set
      */
     public void setLoginMessage(LoginMessage loginMessage) {
         this.loginMessage = loginMessage;
     }
-    
-    /**
-     * @return the eTag
-     */
-    public String geteTag() {
-        return eTag;
-    }
-
-    /**
-     * @param eTag the eTag to set
-     */
-    public void seteTag(String eTag) {
-        this.eTag = eTag;
-    }
-    
-    /**
-     * @return the created
-     */
-    public Date getCreated() {
-        return created;
-    }
-
-    /**
-     * @param created the created to set
-     */
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-    
-    /**
-     * @return the modified
-     */
-    public Date getModified() {
-        return modified;
-    }
-
-    /**
-     * @param modified the modified to set
-     */
-    public void setModified(Date modified) {
-        this.modified = modified;
-    }
-    
-    /**
-     * @return the category
-     */
-    public String getCategory() {
-        return category;
-    }
-    
-    /**
-     * @param category the category to set
-     */
-    public void setCategory(String category) {
-        this.category = category;
-    }
-    
-    /**
-     * @return the uri
-     */
-    public String getUri() {
-        return uri;
-    }
-
-    /**
-     * @param uri the uri to set
-     */
-    public void setUri(String uri) {
-        this.uri = uri;
-    }    
 
     @Override
     public String toString() {
@@ -209,12 +123,10 @@ public class LoginDetail implements Serializable {
     public int hashCode() {
         return HashCodeBuilder.reflectionHashCode(this);
     }
-    
+
+    @Override
     public String toJsonString() {
         return System.getProperty("line.separator")
-            + new GsonBuilder()
-                .setPrettyPrinting()
-                .disableHtmlEscaping()
-                .create().toJson(this);
+                + new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create().toJson(this);
     }
 }
--- oneview-sdk-java-lib/src/main/java/com/hp/ov/sdk/dto/security/login/LoginDomain.java
+++ oneview-sdk-java-lib/src/main/java/com/hp/ov/sdk/dto/security/login/LoginDomain.java
@@ -15,150 +15,61 @@
  */
 package com.hp.ov.sdk.dto.security.login;
 
-import java.io.Serializable;
-import java.util.Date;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-public class LoginDomain implements Serializable{
-    
+public class LoginDomain extends BaseLoginResource {
+
     private static final long serialVersionUID = 1L;
-    
-    private String type;    
+
     private String loginDomain;
     private String authProtocol;
     private String name;
-    private String eTag;
-    private Date created;
-    private Date modified;
-    private String category;   
-    private String uri;
-    
-    /**
-     * @return the type
-     */
-    public String getType() {
-        return type;
-    }
-    
-    /**
-     * @param type the type to set
-     */
-    public void setType(String type) {
-        this.type = type;
-    }
-    
+
     /**
      * @return the loginDomain
      */
     public String getLoginDomain() {
         return loginDomain;
     }
-    
+
     /**
-     * @param loginDomain the loginDomain to set
+     * @param loginDomain
+     *            the loginDomain to set
      */
     public void setLoginDomain(String loginDomain) {
         this.loginDomain = loginDomain;
     }
-    
+
     /**
      * @return the authProtocol
      */
     public String getAuthProtocol() {
         return authProtocol;
     }
-    
+
     /**
-     * @param authProtocol the authProtocol to set
+     * @param authProtocol
+     *            the authProtocol to set
      */
     public void setAuthProtocol(String authProtocol) {
         this.authProtocol = authProtocol;
     }
-    
+
     /**
      * @return the name
      */
     public String getName() {
         return name;
     }
-    
+
     /**
-     * @param name the name to set
+     * @param name
+     *            the name to set
      */
     public void setName(String name) {
         this.name = name;
-    }
-    
-    /**
-     * @return the eTag
-     */
-    public String geteTag() {
-        return eTag;
-    }
-
-    /**
-     * @param eTag the eTag to set
-     */
-    public void seteTag(String eTag) {
-        this.eTag = eTag;
-    }
-    
-    /**
-     * @return the created
-     */
-    public Date getCreated() {
-        return created;
-    }
-
-    /**
-     * @param created the created to set
-     */
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-    
-    /**
-     * @return the modified
-     */
-    public Date getModified() {
-        return modified;
-    }
-
-    /**
-     * @param modified the modified to set
-     */
-    public void setModified(Date modified) {
-        this.modified = modified;
-    }
-    
-    /**
-     * @return the category
-     */
-    public String getCategory() {
-        return category;
-    }
-    
-    /**
-     * @param category the category to set
-     */
-    public void setCategory(String category) {
-        this.category = category;
-    }
-    
-    /**
-     * @return the uri
-     */
-    public String getUri() {
-        return uri;
-    }
-
-    /**
-     * @param uri the uri to set
-     */
-    public void setUri(String uri) {
-        this.uri = uri;
     }
 
     @Override
@@ -175,7 +86,5 @@ public class LoginDomain implements Serializable{
     public int hashCode() {
         return HashCodeBuilder.reflectionHashCode(this);
     }
-
-   
 
 }
