197b6b439353508629d8871a87ca96ed5175b105
Sood <priyanka.sood@hpe.com>
2018-04-18 23:13:32 +0530

    Removing extra commented line for default_domain as its not required

--- oneview-sdk-java-lib/src/main/java/com/hp/ov/sdk/rest/http/core/client/SDKConfiguration.java
+++ oneview-sdk-java-lib/src/main/java/com/hp/ov/sdk/rest/http/core/client/SDKConfiguration.java
@@ -30,7 +30,6 @@ public class SDKConfiguration {
 
     private static final int DEFAULT_MESSAGE_BUS_PORT = 5671;
     private static final ApiVersion DEFAULT_API_VERSION = ApiVersion.V_300;
-    // private static final String DEFAULT_DOMAIN = "LOCAL";
 
     private static final String[] MANDATORY_FIELDS = {
             SDKConfiguration.USERNAME,
